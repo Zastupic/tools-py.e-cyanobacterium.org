4bffbc068fc7fe61f9f1df67419f3f1c4c79e484
Tomáš Zavřel <chalif@email.cz>
2023-06-22 21:06:51 +0200

    initial commit3

--- website/__init__.py
+++ website/__init__.py
@@ -1,31 +1,26 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import IMAGES
-# needed for database - refering
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 from os import path
 
-# creating database (as object)
 db = SQLAlchemy()
 DB_NAME = "database.db"
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'tif', 'tiff', 'gif', 'bmp'}
 
+images = UploadSet("images", IMAGES)
 
 def create_app():
     app = Flask(__name__)
-    # seret key for our app, never to be shown to anybody
-    # creating database (as object)
     app.config['SECRET_KEY'] = 'blablabla' 
-    app.config["UPLOADED_PHOTOS_DEST"] = "static/img"
-    # defining location of database (f'sqlite:///{DB_NAME}) - in website folder (where __init__.py is)
-    # f: f-string, 'functional' way of string writing - whatever will be in the {} will be evaluated as string
+    app.config["UPLOADED_IMAGES_DEST"] = UPLOAD_FOLDER
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    # initializing database - telling the database that it will be used by our app
     db.init_app(app) 
 
-    # we need to import routes defined elsewhere 
     from .views import views
     from .auth import auth
-    from .cell_count import cell_counting
+    from .cell_count import cell_count
     from .clear_uploads import clear_uploads
 
     app.register_blueprint(views, url_prefix='/')
@@ -33,6 +28,8 @@ def create_app():
     app.register_blueprint(cell_count, url_prefix='/')
     app.register_blueprint(clear_uploads, url_prefix='/')
 
+    from .models import User
+    
     #### DATABASE ####
     with app.app_context(): # creating the database
         db.create_all()
