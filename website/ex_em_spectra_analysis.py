8f209bc49a1b53bb054e21bbec20ae14debdd3f4
Tomáš Zavřel <chalif@email.cz>
2024-01-02 15:06:19 +0100

    login+sign up

--- website/ex_em_spectra_analysis.py
+++ website/ex_em_spectra_analysis.py
@@ -6,4 +6,8 @@ ex_em_spectra_analysis = Blueprint('ex_em_spectra_analysis', __name__)
 
 @ex_em_spectra_analysis.route('/ex_em_spectra_analysis', methods=['GET', 'POST'])
 def analyze_ex_em_spectra():
-    return render_template("ex_em_spectra_analysis.html")
+    if current_user.is_authenticated:
+        return render_template("ex_em_spectra_analysis.html")
+    else:
+        flash('Please login', category='error')
+        return redirect("/login")
